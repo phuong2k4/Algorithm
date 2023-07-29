c4a1786550f37ebd1188bd291079c7c924f1ba28
IVO JONES <phuongsepfg@gmail.com>
2023-07-29 22:27:52 +0700

    .

---
+++ SapXep/QuickSort.cpp
@@ -0,0 +1,43 @@
+#include<conio.h>
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int patition(int a[],int l,int r)
+{
+    int pivot = a[r];
+    int i=l-1;
+    for (int j=l;j<r;i++){
+        if(a[j]<=pivot){
+            ++i;
+            swap(a[i],a[j]);
+        }
+    }
+    ++i;
+    swap(a[i],a[r]);
+    return i;
+    
+    
+}
+
+void quicksort(int a[],int l,int r)
+{
+    if(l>=r) return ;
+    int p = patition(a,l,r);
+    quicksort(a,l,p-1);
+    quicksort(a,p+1,r);
+}
+
+int main(){
+    int a[1000],n;cin>> n;
+    srand(time(NULL));
+    for (int i=0;i<n;i++){
+        a[i]=rand()%1000;
+    }
+    quicksort(a,0,n-1);
+    for (int i=0;i<n;i++){
+        cout << a[i]<< " ";
+    }
+    return 0;
+}
