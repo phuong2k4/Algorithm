1e9313e1854a1c99a351d24febcab243d3813b08
IVO JONES <phuongsepfg@gmail.com>
2023-08-02 22:55:42 +0700

    .

---
+++ DeQuy/Quaylui.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<algorithm>
+#include<conio.h>
+
+
+using namespace std;
+
+int n,X[100];
+int inkq(){
+    for(int i=1;i<=n;i++){
+        cout << X[i];
+    }
+    cout << endl;
+}
+void Try(int i){
+     for(int j=0;j<=1;j++){
+        X[i]=j;
+        if(i == n){
+            inkq();
+        }
+        else{
+            Try(i+1);
+        }
+    }
+
+}
+
+int main(){
+    
+    cin>> n;
+    Try(1);
+    return 0;
+}
